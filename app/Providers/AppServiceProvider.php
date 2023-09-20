72064404d0b3a84ce9dc6196b4bc7028cb964e71
yoshihiro-seo <129734191+yoshihiro-seo@users.noreply.github.com>
2023-09-20 13:50:18 +0900

    20230920

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app['request']->server->set('HTTPS','on');
         if (\App::environment(['production']) || \App::environment(['develop'])) {
             \URL::forceScheme('https');
         }
